3176aba326d54f7ccdc902f2edd08da81e867862
Priti Sharma <pritisharma@Shaktis-MacBook-Air-2.local>
2023-07-18 23:29:35 +0530

    add the header and dynamic the nav bar of the product detail

--- productsDetails.js
+++ productsDetails.js
@@ -13,16 +13,17 @@ fetch(url)
 .then(res => res.json())
 .then((response) => {
     console.log(response);
-
-
     document.getElementById("product_title").innerHTML = response.title;
     document.getElementById("product_description").innerHTML = response.description;
     document.getElementById("product_price").innerHTML = response.price;
+    });
+
+    fetch("http://127.0.0.1:8080/header.html")
+    // .then((res) => {
+    //     return res.text()
+    // })
+
+    .then((res) => res.text())
+    .then(res => 
+        document.getElementById("header-bar").innerHTML = res);
     
-
-
-
-    
-     
-
-});
